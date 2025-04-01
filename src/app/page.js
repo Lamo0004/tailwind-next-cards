7d34bb20733d70cfe62aaa501e38e7929b1780b5
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-04-01 10:50:58 +0200

    card designet med hover og dark-mode

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <main>
       <ThemeToggle />
-      <ul>
+      <ul className="bg-white p-4 rounded-2xl dark:bg-gray-900 shadow-2xl">
         {data.map((item, i) => (
           <Card key={i} {...item} />
         ))}
@@ -33,23 +33,15 @@ export default function Home() {
 
 function Card({ title, category }) {
   return (
-    <li>
-      <a href="#0">
+    <li className="group .max-w-[400px] w-[400px] hover:bg-slate-100 dark:hover:bg-slate-800 rounded-2xl">
+      <a className="flex items-center gap-4 p-4" href="#0">
         <div className="aspect-square w-10 rounded-md bg-[#cbd5e1]"></div>
         <div>
-          <p>{title}</p>
-          <small>{category}</small>
+          <p className="font-bold dark:text-white">{title}</p>
+          <small className="dark:text-white">{category}</small>
         </div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="currentColor"
-            d="m12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8l-8-8z"
-          ></path>
+        <svg className="hidden ml-auto text-purple-600 dark:text-purple-400 group-hover:block" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+          <path fill="currentColor" d="m12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8l-8-8z"></path>
         </svg>
       </a>
     </li>
